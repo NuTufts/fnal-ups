dc0b559f62bc55261e7935b045280a576d149604
rasmus <rasmus>
1997-07-25 17:13:38 +0000

    modified ups_list_add to be as fast as ups_list_insert

--- src/ups_list.c
+++ src/ups_list.c
@@ -16,6 +16,12 @@
  *
  * MODIFICATIONS:
  *       23-Jun-1997, LR, first
+ *       25-Jul-1997, LR, ups_list_add is now as fast as ups_list_insert:
+ *                        ups_list_add will now return the last element.
+ *                        ups_list_insert will return the first element.
+ *                        In that way, successive calls using previous
+ *                        return will be fast.  
+ *                        Changed 'bot' to 'last' and 'top' to 'first'.
  *
  ***********************************************************************/
 
@@ -46,23 +52,23 @@
  *
  * Will create a new ups list and add first item.
  *
- * Input : void *, a list item
+ * Input : void *, a data element
  * Output: none
  * Return: t_ups_list_item *, pointer to top of list or NULL
  */
 t_ups_list_item *ups_list_new( void *data_ptr )
 {
-  t_ups_list_item *l_top = NULL;
+  t_ups_list_item *l_first = NULL;
 
-  l_top = (t_ups_list_item *)malloc( sizeof( t_ups_list_item ) );
+  l_first = (t_ups_list_item *)malloc( sizeof( t_ups_list_item ) );
 
-  if ( !l_top ) return NULL;
+  if ( !l_first ) return NULL;
   
-  l_top->data = data_ptr;
-  l_top->prev = NULL;
-  l_top->next = NULL;
+  l_first->data = data_ptr;
+  l_first->prev = NULL;
+  l_first->next = NULL;
 
-  return( l_top );
+  return( l_first );
 }	
 
 /*-----------------------------------------------------------------------
@@ -80,11 +86,11 @@ t_ups_list_item *ups_list_free( t_ups_list_item *list_ptr, char copt )
 {
   t_ups_list_item *l_ptr = NULL;
   t_ups_list_item *l_tmp = NULL;
-  t_ups_list_item *l_top = ups_list_top( list_ptr );
+  t_ups_list_item *l_first = ups_list_first( list_ptr );
 
-  if ( !l_top ) return NULL;
+  if ( !l_first ) return NULL;
 
-  l_ptr = l_top;
+  l_ptr = l_first;
   while( l_ptr ) {
     if ( copt == 'd' && l_ptr->data ) free ( l_ptr->data );
     l_tmp = l_ptr;
@@ -99,28 +105,31 @@ t_ups_list_item *ups_list_free( t_ups_list_item *list_ptr, char copt )
 /*-----------------------------------------------------------------------
  * ups_list_insert
  *
- * Will add an item to the top of the list.
+ * Will add an item as the first item to the list.
  *
  * Input : t_ups_list_item *, pointer to a list.
  * Output: none
- * Return: t_ups_list_item *, pointer to top of list or NULL
+ * Return: t_ups_list_item *, pointer to the first item of the list or NULL
+ *
+ * NOTE: if passed list pointer, in successive calls, is the first item,
+ * it should be pretty fast.
  */
 t_ups_list_item *ups_list_insert( t_ups_list_item *list_ptr, void *data_ptr )
 {
-  t_ups_list_item *l_top = NULL;
+  t_ups_list_item *l_first = NULL;
   t_ups_list_item *l_new = NULL;
 
-  l_top = ups_list_top( list_ptr );
+  l_first = ups_list_first( list_ptr );
 
-  if ( !l_top ) return NULL;
+  if ( !l_first ) return NULL;
   
   l_new = (t_ups_list_item *)malloc( sizeof( t_ups_list_item ) );
 
   if ( !l_new ) return NULL;
 
   l_new->data = data_ptr;
-  l_top->prev = l_new;
-  l_new->next = l_top;
+  l_first->prev = l_new;
+  l_new->next = l_first;
   l_new->prev = NULL;
 
   return l_new;
@@ -129,31 +138,34 @@ t_ups_list_item *ups_list_insert( t_ups_list_item *list_ptr, void *data_ptr )
 /*-----------------------------------------------------------------------
  * ups_list_add
  *
- * Will add an item to the end of the list.
+ * Will add an item as the last item to the list.
  *
  * Input : t_ups_list_item *, pointer to a list.
  * Output: none
- * Return: t_ups_list_item *, pointer to top the list or NULL
+ * Return: t_ups_list_item *, pointer to the last item of the list or NULL
+ *
+ * NOTE: if passed list pointer, in successive calls, is the last item,
+ * it should be pretty fast.
  */
 t_ups_list_item *ups_list_add( t_ups_list_item *list_ptr, void *data_ptr )
 {
-  t_ups_list_item *l_bot = NULL;
+  t_ups_list_item *l_last = NULL;
   t_ups_list_item *l_new = NULL;
 
-  l_bot = ups_list_bot( list_ptr );
+  l_last = ups_list_last( list_ptr );
 
-  if ( !l_bot ) return NULL;
+  if ( !l_last ) return NULL;
   
   l_new = (t_ups_list_item *)malloc( sizeof( t_ups_list_item ) );
 
   if ( !l_new ) return NULL;
 
   l_new->data = data_ptr;
-  l_bot->next = l_new;
-  l_new->prev = l_bot;
+  l_last->next = l_new;
+  l_new->prev = l_last;
   l_new->next = NULL;
 
-  return ups_list_top( l_new );
+  return l_new;
 }
 
 /*-----------------------------------------------------------------------
@@ -172,11 +184,11 @@ t_ups_list_item *ups_list_delete( t_ups_list_item *list_ptr, void *data_ptr, cha
 {
   t_ups_list_item *l_ptr = NULL;
   t_ups_list_item *l_prev = NULL;
-  t_ups_list_item *l_top = NULL;
+  t_ups_list_item *l_first = NULL;
 
-  l_top = ups_list_top( list_ptr );
+  l_first = ups_list_first( list_ptr );
   
-  for ( l_ptr = l_top; l_ptr; l_ptr = l_ptr->next ) {
+  for ( l_ptr = l_first; l_ptr; l_ptr = l_ptr->next ) {
     l_prev = l_ptr->prev;
 
     if ( l_ptr->data == data_ptr ) {
@@ -188,11 +200,11 @@ t_ups_list_item *ups_list_delete( t_ups_list_item *list_ptr, void *data_ptr, cha
 	if ( l_prev ) l_prev->next = NULL;
       }
       if ( copt == 'd' && l_ptr->data ) free( l_ptr->data );
-      if ( l_ptr == l_top ) {
-	l_top = l_ptr->next;
+      if ( l_ptr == l_first ) {
+	l_first = l_ptr->next;
       }
       free( l_ptr );
-      return l_top;
+      return l_first;
     }
   }
 
@@ -201,15 +213,15 @@ t_ups_list_item *ups_list_delete( t_ups_list_item *list_ptr, void *data_ptr, cha
 }
 
 /*-----------------------------------------------------------------------
- * ups_list_top
+ * ups_list_first
  *
- * Will return top item of passed list.
+ * Will return first item of passed list.
  *
  * Input : t_ups_list_item *, pointer to a list
  * Output: none
- * Return: t_ups_list_item *, pointer to top of list
+ * Return: t_ups_list_item *, pointer to first item of list
  */
-t_ups_list_item *ups_list_top( t_ups_list_item *list_ptr )
+t_ups_list_item *ups_list_first( t_ups_list_item *list_ptr )
 {
   t_ups_list_item *l_ptr = NULL;
   
@@ -221,15 +233,15 @@ t_ups_list_item *ups_list_top( t_ups_list_item *list_ptr )
 }
 
 /*-----------------------------------------------------------------------
- * ups_list_bot
+ * ups_list_last
  *
- * Will return bottom item of passed list.
+ * Will return last item of passed list.
  *
  * Input : t_ups_list_item *, pointer to a list
  * Output: none
- * Return: t_ups_list_item *, pointer to end of list
+ * Return: t_ups_list_item *, pointer to last item of list
  */
-t_ups_list_item *ups_list_bot( t_ups_list_item *list_ptr )
+t_ups_list_item *ups_list_last( t_ups_list_item *list_ptr )
 {
   t_ups_list_item *l_ptr = NULL;
   
@@ -243,4 +255,12 @@ t_ups_list_item *ups_list_bot( t_ups_list_item *list_ptr )
 /*
  * Definition of private functions.
  */
+
+
+
+
+
+
+
+
 
