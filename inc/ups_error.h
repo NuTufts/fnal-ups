cc7ce77c49db6ae8896371c11a3d61af799b65a9
fagan <fagan>
1997-07-31 19:28:56 +0000

    Added ups_invalid_argument for invalid command line flags/options

--- inc/ups_error.h
+++ inc/ups_error.h
@@ -32,17 +32,18 @@
  */
 
 /* Error Messages */
-#define UPS_INVALID         -1
-#define UPS_SUCCESS         0
-#define UPS_OPEN_FILE       1
-#define UPS_READ_FILE       2
-#define UPS_INVALID_KEYWORD 3
-#define UPS_NO_DATABASE     4
-#define UPS_TIME            5
-#define UPS_NAME_TOO_LONG   6
-#define UPS_NO_STAT_DIR     7
-#define UPS_WRITE_FILE      8
-#define UPS_NERR            9
+#define UPS_INVALID           -1
+#define UPS_SUCCESS           0
+#define UPS_OPEN_FILE         1
+#define UPS_READ_FILE         2
+#define UPS_INVALID_KEYWORD   3
+#define UPS_NO_DATABASE       4
+#define UPS_TIME              5
+#define UPS_NAME_TOO_LONG     6
+#define UPS_NO_STAT_DIR       7
+#define UPS_WRITE_FILE        8
+#define UPS_NERR              9
+#define UPS_INVALID_ARGUMENT  10
 
 #define UPS_ERROR           1
 #define UPS_FATAL           "ERROR"
