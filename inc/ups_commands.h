ecddd687f92d9b9851a64192f213f06028addda5
berman <berman>
1997-08-18 21:30:29 +0000

    prototypes for ups commands

---
+++ inc/ups_commands.h
@@ -0,0 +1,52 @@
+/************************************************************************
+ *
+ * FILE:
+ *       ups_commands.h
+ * 
+ * DESCRIPTION: 
+ *       Define all necessary command prototypes etc.
+ *
+ * AUTHORS:
+ *       Eileen Berman
+ *       David Fagan
+ *       Lars Rasmussen
+ *
+ *       Fermilab Computing Division
+ *       Batavia, Il 60510, U.S.A.
+ *
+ * MODIFICATIONS:
+ *       18-Aug-1997, EB, first
+ *
+ ***********************************************************************/
+
+#ifndef _UPS_COMMANDS_H_
+#define _UPS_COMMANDS_H_
+
+/* standard include files, if needed for .h file */
+
+/* ups specific include files, if needed for .h file */
+
+/*
+ * Constans.
+ */
+
+/*
+ * Types.
+ */
+
+/*
+ * Declaration of public functions.
+ */
+void ups_c_list( t_ups_command *a_command_line );
+
+
+/*
+ * Declaration of private globals.
+ */
+
+/*
+ * Declarations of public variables.
+ */
+
+
+#endif /* _UPS_COMMANDS_H_ */
