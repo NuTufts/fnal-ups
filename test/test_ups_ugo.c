83eac27490acb22749f1f48ed1c25b9fa0986aad
fagan <fagan>
1997-09-05 14:06:12 +0000

    Changed to use the upsugo_dump, program rather simple minded now.

--- test/test_ups_ugo.c
+++ test/test_ups_ugo.c
@@ -56,96 +56,15 @@ int main (argc,argv)
 */
         uc=upsugo_env("TEST","AacCdfghKtmMNoOPqrTuU");
         if (uc) 
-	fprintf(stdout,"product: %s\n",uc->ugo_product);
-	fprintf(stdout,"version: %s\n",uc->ugo_version);
+	{ upsugo_dump(uc);
+          upsugo_free(uc);
+        }
         uc=0;
 while ((uc = upsugo_next(argc,argv,"AacCdfghKtmMNoOPqrTuU")) != 0 )
-{
-	fprintf(stdout,"product: %s\n",uc->ugo_product);
-	fprintf(stdout,"version: %s\n",uc->ugo_version);
-	if (uc->ugo_C) {
-	fprintf(stdout,"Don't do Configure\n");
-	}
-	if (uc->ugo_M) {
-	fprintf(stdout,"Table file dir: %s\n",uc->ugo_tablefiledir);
-	}
-	if (uc->ugo_m) {
-	fprintf(stdout,"Table file: %s\n",uc->ugo_tablefile);
-	}
-	if (uc->ugo_N) {
-	fprintf(stdout,"Any \"N\" file: %s\n",uc->ugo_anyfile);
-	}
-	if (uc->ugo_O) {
-	fprintf(stdout,"set UPS_OPTIONS =: %s\n",uc->ugo_options);
-	}
-	if (uc->ugo_P) {
-	fprintf(stdout,"Override Product name=: %s\n",uc->ugo_override);
-	}
-	if (uc->ugo_r) {
-	fprintf(stdout,"set PROD_DIR =: %s\n",uc->ugo_productdir);
-	}
-	if (uc->ugo_T) {
-	fprintf(stdout,"set archive file=: %s\n",uc->ugo_archivefile);
-	}
-	if (uc->ugo_u) {
-	fprintf(stdout,"Uncompile first\n");
-	}
-	if (uc->ugo_U) {
-	fprintf(stdout,"set UPS_DIR =: %s\n",uc->ugo_upsdir);
-	}
-	if (uc->ugo_flavor) {
-	    fprintf(stdout,"Flavors:\n");
-            print_list( uc->ugo_flavor );
-	    fprintf(stdout,"\n");
-	}
-	if (uc->ugo_host) {
-	    fprintf(stdout,"host\n");
-            print_list( uc->ugo_host );
-	    fprintf(stdout,"\n");
-	}
-	if (uc->ugo_auth) {
-	    fprintf(stdout,"auth\n");
-            print_list( uc->ugo_auth );
-	    fprintf(stdout,"\n");
-	}
-	if (uc->ugo_key) {
-	    fprintf(stdout,"key\n");
-            print_list( uc->ugo_key );
-	    fprintf(stdout,"\n");
-	}
-	if (uc->ugo_chain) {
-	    fprintf(stdout,"chain\n");
-            print_list( uc->ugo_chain );
-	    fprintf(stdout,"\n");
-	}
-	if (uc->ugo_qualifiers) {
-	    fprintf(stdout,"qualifiers\n");
-            print_list( uc->ugo_qualifiers );
-	    fprintf(stdout,"\n");
-	}
-}
+      { upsugo_dump(uc); }
 /*
 	if (status != UPS_SUCCESS) fprintf(stderr," %s \n", UPS_ERRTXT[status]);
 */
 	exit(status);
 
-}
-
-void print_list( t_upslst_item *list_ptr )
-{
-  t_upslst_item *l_ptr;
-  int count = 0;
-
-  /*
-   * Note use of upslst_first(), to be sure to start from first item
-   */
-  
-  for ( l_ptr = upslst_first( list_ptr ); l_ptr; l_ptr = l_ptr->next, count++ ) {
-/* fprintf(stdout,"ref count %d\n",upsmem_get_refctr((void *)(l_ptr))); */
-fprintf(stdout,"ref count %d\n",upsmem_get_refctr(l_ptr->data));
-
-    printf( "%03d: p=%08x, i=%08x, n=%08x, data=%s\n",
-	    count, (int)l_ptr->prev, (int)l_ptr,
-	    (int)l_ptr->next, (char *)l_ptr->data );    
-  }
 }
