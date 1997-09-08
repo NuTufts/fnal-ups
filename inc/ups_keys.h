7cf8075e0d085fb8fdbe048cfb43bf467cca664b
rasmus <rasmus>
1997-09-08 20:55:36 +0000

    routines to handle ups keys

---
+++ inc/ups_keys.h
@@ -0,0 +1,121 @@
+/***********************************************************************
+ *
+ * FILE:
+ *       ups_keys.h
+ * 
+ * DESCRIPTION: 
+ *       Translations for ups keys
+ *
+ * AUTHORS:
+ *       Eileen Berman
+ *       David Fagan
+ *       Lars Rasmussen
+ *
+ *       Fermilab Computing Division
+ *       Batavia, Il 60510, U.S.A.                                              
+ *
+ * MODIFICATIONS:
+ *       25-aug-1997, LR, first
+ *
+ ***********************************************************************/
+
+#ifndef _UPS_KEYS_H_
+#define _UPS_KEYS_H_
+
+/*
+ * Standard include files, if needed for .h file
+ */
+
+/*
+ * ups specific include files, if needed for .h file
+ */
+#include "ups_types.h"
+
+/*
+ * Constants.
+ */
+
+/* enum of known keys (changes here should be reflected in g_ups_keys) */
+enum e_upskey_key {
+  e_key_file = 0,
+  e_key_product,
+  e_key_version,
+  e_key_chain,
+  e_key_ups_db_version,
+
+  e_key_flavor,
+  e_key_qualifiers,
+  e_key_declarer,
+  e_key_declared,
+  e_key_prod_dir,
+  e_key_ups_dir,
+  e_key_table_dir,
+  e_key_table_file,
+  e_key_archive_file,
+  e_key_authorized_nodes,
+  e_key_description,
+  e_key_statistics,
+
+  e_key_db_dir,
+  e_key_action,
+  e_key_unknown,
+  
+  e_key_group,
+  e_key_common,
+  e_key_end,
+
+  e_key_count
+};
+
+/*
+ * Types.
+ */
+
+typedef struct upskey_map {
+  int           ikey;    /* enum of key (corresponding to e_upskey_key) */
+  char          *key;    /* string of key */
+  int           p_index; /* index into product structure (t_ups_product) */
+  int           i_index; /* index into instance structure (t_ups_instance) */
+  unsigned int  flag;    /* flags, see ups_keys.c */
+} t_upskey_map;
+
+typedef union ups_product_u {
+  t_ups_product  prod;
+  char           *arr[e_key_count];
+} t_ups_product_u;
+
+typedef union ups_instance_u {
+  t_ups_instance inst;
+  char           *arr[e_key_count];
+} t_ups_instance_u;
+
+/* some convenient macros */
+
+#define UPSKEY_PROD2ARR( prod ) (((t_ups_product_u *)prod)->arr)
+#define UPSKEY_INST2ARR( inst ) (((t_ups_instance_u *)inst)->arr)
+
+#define UPSKEY_ISIN_VERSION( flag ) (flag&0x0001 ? 1 : 0)
+#define UPSKEY_ISIN_TABLE( flag )   (flag&0x0010 ? 1 : 0)
+#define UPSKEY_ISIN_CHAIN( flag )   (flag&0x0100 ? 1 : 0)
+#define UPSKEY_ISIN_CONFIG( flag )  (flag&0x1000 ? 1 : 0)
+
+/*
+ * Declaration of public functions.
+ */
+t_upskey_map *upskey_get_map( const char * const skey );
+char         *upskey_inst_getval( t_ups_instance * const inst,
+				  const char * const skey );
+char         *upskey_inst_setval( t_ups_instance * const inst,
+				  const char * const skey, const char * const sval );
+void         upskey_inst_print( const t_ups_instance * const prod );
+char         *upskey_prod_getval( t_ups_product * const prod,
+				  const char * const skey );
+char         *upskey_prod_setval( t_ups_product * const prod,
+				  const char * const skey, const char * const sval );
+void         upskey_prod_print( const t_ups_product * const prod );
+     
+/*
+ * Declarations of public variables.
+ */
+
+#endif /* _UPS_KEYS_H_ */
---
+++ src/ups_keys.c
@@ -0,0 +1,232 @@
+/***********************************************************************
+ *
+ * FILE:
+ *       ups_keys.c
+ * 
+ * DESCRIPTION: 
+ *       Translations for ups keys
+ *
+ * AUTHORS:
+ *       Eileen Berman
+ *       David Fagan
+ *       Lars Rasmussen
+ *
+ *       Fermilab Computing Division
+ *       Batavia, Il 60510, U.S.A.                                              
+ *
+ * MODIFICATIONS:
+ *       25-aug-1997, LR, first
+ *
+ ***********************************************************************/
+/* standard include files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* ups specific include files */
+#include "ups_types.h"
+#include "ups_memory.h"
+#include "ups_keys.h"
+
+/*
+ * Definition of public variables
+ */
+
+/*
+ * Declaration of private functions
+ */
+
+/*
+ * Definition of global variables
+ */
+#define NO INVALID_INDEX
+
+/*
+ * g_key_map.
+ *
+ * It goes like: 'enum key', 'string key', 'index to product structure',
+ *               'index to instance structure', 'flag'.
+ * where flag:
+ * <byte>: <description>
+ *   0   : key can be in a version file
+ *   1   : key can be in a table file
+ *   2   : key can be in a chain file
+ *   3   : key can be in a config file
+ *   4   : spare
+ *   5   : spare
+ *   6   : spare
+ *   7   : spare
+ */
+static t_upskey_map g_key_map[] =
+{
+  {  0, "FILE",             0,    NO,   0x1111 },
+  {  1, "PRODUCT",          1,     0,   0x0111 },
+  {  2, "VERSION",          2,     1,   0x0111 }, 
+  {  3, "CHAIN",            3,     4,   0x0110 },
+  {  4, "UPS_DB_VERSION",   4,    NO,   0x1111 },
+  {  5, "FLAVOR",           NO,    2,   0x0111 },
+  {  6, "QUALIFIERS",       NO,    3,   0x0111 },
+  {  7, "DECLARER",         NO,    5,   0x0101 },
+  {  8, "DECLARED",         NO,    6,   0x0101 },
+  {  9, "PROD_DIR",         NO,    7,   0x0001 },
+  { 10, "UPS_DIR",          NO,    8,   0x0001 },
+  { 11, "TABLE_DIR",        NO,    9,   0x0001 },
+  { 12, "TABLE_FILE",       NO,   10,   0x0001 },
+  { 13, "ARCHIVE_FILE",     NO,   11,   0x0001 },
+  { 14, "AUTHORIZED_NODES", NO,   12,   0x1001 },
+  { 15, "DESCRIPTION",      NO,   13,   0x0100 },
+  { 16, "STATISTICS",       NO,   14,   0x1001 },
+  { 17, "DB_DIR",           NO,   15,   0x0000 },
+  { 18, "ACTION",           NO,   16,   0x0010 },
+  { 19, "UNKNOWN",          NO,   17,   0x0011 },
+
+  { 20, "GROUP:",           NO,   NO,   0x0010 },
+  { 21, "COMMON:",          NO,   NO,   0x0010 },
+  { 22, "END:",             NO,   NO,   0x0010 },
+
+  { 23,0,0,0,0 },
+};
+
+/*
+ * Definition of public functions
+ */
+
+/*-----------------------------------------------------------------------
+ * upskey_get_map
+ *
+ * Will return a map (t_upskey_map) for passed key.
+ *
+ * Input : char *, string of key.
+ * Output: none.
+ * Return: t_upskey_map *, corresponding map, or 0.
+ */
+t_upskey_map *upskey_get_map( const char * const str )
+{
+  t_upskey_map *keys;
+
+  /* for now, just a linear search */
+  
+  for ( keys = g_key_map; keys->key; keys++ ) {
+    if ( !strcmp( keys->key, str ) )
+      return keys;
+  }
+
+  return 0;
+}
+
+/*-----------------------------------------------------------------------
+ * upskey_inst_getval
+ *
+ * Will return an instance value corresponding to passed key
+ *
+ * Input : t_ups_instance *, an instance.
+ *         char *, string of key.
+ * Output: none.
+ * Return: char *, an instance value, or 0.
+ */
+char *upskey_inst_getval( t_ups_instance * const inst, const char * const skey )
+{
+  t_upskey_map *key = upskey_get_map( skey );
+  if ( key && key->i_index != NO )
+    return UPSKEY_INST2ARR( inst )[key->i_index];
+  else
+    return 0;  
+}
+
+/*-----------------------------------------------------------------------
+ * upskey_inst_setval
+ *
+ * Will set an instance value corresponding to passed key.
+ *
+ * Input : t_ups_instance *, an instance.
+ *         char *, string of key.
+ *         char *, value.
+ * Output: none.
+ * Return: char *, return value, or 0.
+ */
+char *upskey_inst_setval( t_ups_instance * const inst,
+			  const char * const skey, const char * const sval )
+{
+  t_upskey_map *key = upskey_get_map( skey );
+  if ( key && key->i_index != NO ) {
+    char *new_val = (char *)upsmem_malloc( strlen( sval ) + 1 );
+    strcpy( new_val, sval );
+    return ( UPSKEY_INST2ARR( inst )[key->i_index] = new_val );
+  }
+  else
+    return 0;  
+}
+
+void upskey_inst_print( const t_ups_instance * const inst )
+{
+  t_upskey_map *keys;
+  int ix;
+  
+  if ( !inst ) return;
+  
+  for ( keys = g_key_map; keys->key; keys++ ) {
+    if ( (ix = keys->i_index) != NO && UPSKEY_INST2ARR( inst )[ix] )
+      printf( "%s = %s\n", keys->key, UPSKEY_INST2ARR( inst )[ix] );    
+  }
+}
+
+/*-----------------------------------------------------------------------
+ * upskey_prod_getval
+ *
+ * Will return a product value corresponding to passed key
+ *
+ * Input : t_ups_product *, a product.
+ *         char *, string of key.
+ * Output: none.
+ * Return: char *, a product value, or 0.
+ */
+char *upskey_prod_getval( t_ups_product * const prod, const char * const skey )
+{
+  t_upskey_map *key = upskey_get_map( skey );
+  if ( key && key->p_index != NO )
+    return UPSKEY_PROD2ARR( prod )[key->p_index];
+  else
+    return 0;  
+}
+
+/*-----------------------------------------------------------------------
+ * upskey_prod_setval
+ *
+ * Will set a product value corresponding to passed key.
+ *
+ * Input : t_ups_product *, a product.
+ *         char *, string of key.
+ *         char *, value.
+ * Output: none.
+ * Return: char *, return value, or 0.
+ */
+char *upskey_prod_setval( t_ups_product * const prod,
+			  const char * const skey, const char * const sval )
+{
+  t_upskey_map *key = upskey_get_map( skey );
+  if ( key && key->p_index != NO ) {
+    char *new_val = (char *)upsmem_malloc( strlen( sval ) + 1 );
+    strcpy( new_val, sval );
+    return ( UPSKEY_PROD2ARR( prod )[key->p_index] = new_val );
+  }
+  else
+    return 0;  
+}
+
+void upskey_prod_print( const t_ups_product * const prod )
+{
+  t_upskey_map *keys;
+  int ix;
+   
+  if ( !prod ) return;
+  
+  for ( (keys = g_key_map); keys->key; keys++ ) {
+    if ( (ix = keys->p_index) != NO && UPSKEY_PROD2ARR( prod )[ix] ) 
+      printf( "%s = %s\n", keys->key, UPSKEY_PROD2ARR( prod )[ix] );
+  }
+}
+
+/*
+ * Definition of private functions
+ */
