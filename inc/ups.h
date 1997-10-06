26053b81aa0de0d9de2299d2f591602ad6fcb7aa
berman <berman>
1997-10-06 14:43:26 +0000

    include all ups include files

---
+++ inc/ups.h
@@ -0,0 +1,38 @@
+/************************************************************************
+ *
+ * FILE:
+ *       ups.h
+ * 
+ * DESCRIPTION: 
+ *       This file includes all possible ups include files and is to be used
+ *       as a shortcut by the user so they do not have to know the details.
+ *
+ * AUTHORS:
+ *       Eileen Berman
+ *       David Fagan
+ *       Lars Rasmussen
+ *
+ *       Fermilab Computing Division
+ *       Batavia, Il 60510, U.S.A.
+ *
+ * MODIFICATIONS:
+ *       6-Oct-1997, EB, first
+ *
+ ***********************************************************************/
+
+#ifndef _UPS_H_
+#define _UPS_H_
+
+/* all ups specific include files */
+#include "ups_action.h"
+#include "ups_commands.h"
+#include "ups_error.h"
+#include "ups_files.h"
+#include "ups_keys.h"
+#include "ups_list.h"
+#include "ups_match.h"
+#include "ups_memory.h"
+#include "ups_types.h"
+#include "ups_utils.h"
+
+#endif /* _UPS_H_ */
