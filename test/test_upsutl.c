ffa1d4cac948b1e02204c543f7062aed9d49db2e
berman <berman>
1997-10-29 14:29:47 +0000

    remove test for statistics

--- test/test_upsutl.c
+++ test/test_upsutl.c
@@ -52,35 +52,9 @@ int main (void)
   /* First set up the stuff we need - an instance, a database and a command.
      The database, product combination better be someplace writeable */
 
-  char db[] = ".";
-  t_upstyp_instance instance;
-  char command[] = "ups -c -f global thermonuclear_war";
-  t_upslst_item *list_ptr;
+    t_upslst_item *list_ptr;
 
-  instance.product = "middleEast";
-  instance.version = "v1_0";
-  instance.qualifiers = "debug, build";
-  instance.flavor = "global";
-
-  upsutl_statistics(&instance, db, command);
-  if (UPS_ERROR != UPS_SUCCESS) {
-    upserr_output();
-  }
-
-  /* Now append to the file */
-  upsutl_statistics(&instance, db, command);
-  if (UPS_ERROR != UPS_SUCCESS) {
-    upserr_output();
-  }
-
-  /* Now write a new file */
-  instance.product = "WarGames";
-  upsutl_statistics(&instance, db, command);
-  if (UPS_ERROR != UPS_SUCCESS) {
-    upserr_output();
-  }
-
-  /* Now test the function upsutl_get_files */
+   /* Now test the function upsutl_get_files */
   list_ptr = upsutl_get_files("/usrdevel/s1/berman/upsdb/tigger", 
 			      (char *)ANY_MATCH);
 
