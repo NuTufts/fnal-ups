6bf31f1d39538f0e2f2e35098b362c0db51a5b55
fagan <fagan>
1997-11-18 15:00:34 +0000

    Opps, the upsget will have to pass matched product and instance.

--- inc/upsget.h
+++ inc/upsget.h
@@ -23,21 +23,29 @@
 char *upsget_translation( const t_upstyp_matched_product * const ,
                   const t_upsugo_command * const ,
                   char * const );
-char *upsget_prod_dir(const t_upstyp_matched_instance * const ,
+char *upsget_prod_dir(const t_upstyp_matched_product * const,
+                      const t_upstyp_matched_instance * const ,
                       const t_upsugo_command * const );
-char *upsget_product(const t_upstyp_matched_instance * const ,
+char *upsget_product(const t_upstyp_matched_product * const,
+                     const t_upstyp_matched_instance * const ,
                      const t_upsugo_command * const );
-char *upsget_version(const t_upstyp_matched_instance * const ,
+char *upsget_version(const t_upstyp_matched_product * const,
+                     const t_upstyp_matched_instance * const ,
                      const t_upsugo_command * const );
-char *upsget_flavor(const t_upstyp_matched_instance * const ,
+char *upsget_flavor(const t_upstyp_matched_product * const,
+                    const t_upstyp_matched_instance * const ,
                     const t_upsugo_command * const );
-char *upsget_qualifiers(const t_upstyp_matched_instance * const ,
+char *upsget_qualifiers(const t_upstyp_matched_product * const,
+                        const t_upstyp_matched_instance * const ,
                         const t_upsugo_command * const );
-char *upsget_shell(const t_upstyp_matched_instance * const ,
+char *upsget_shell(const t_upstyp_matched_product * const,
+                   const t_upstyp_matched_instance * const ,
                    const t_upsugo_command * const );
-char *upsget_verbose(const t_upstyp_matched_instance * const ,
+char *upsget_verbose(const t_upstyp_matched_product * const,
+                     const t_upstyp_matched_instance * const ,
                      const t_upsugo_command * const );
-char *upsget_options(const t_upstyp_matched_instance * const ,
+char *upsget_options(const t_upstyp_matched_product * const,
+                     const t_upstyp_matched_instance * const ,
                      const t_upsugo_command * const );
 
 #endif /* _UPSGET_H_ */
