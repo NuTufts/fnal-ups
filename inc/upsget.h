2f14cdceaab5a299396266c2adb5b9875a3f1318
fagan <fagan>
1997-11-20 14:54:14 +0000

    added proto for upsget_allout

--- inc/upsget.h
+++ inc/upsget.h
@@ -56,5 +56,9 @@ char *upsget_OS_flavor(const t_upstyp_db * const,
 char *upsget_extended(const t_upstyp_db * const,
                       const t_upstyp_matched_instance * const ,
                       const t_upsugo_command * const );
+char *upsget_allout(const FILE * const,
+                    const t_upstyp_db * const,
+                    const t_upstyp_matched_instance * const,
+                    const t_upsugo_command * const);
 
 #endif /* _UPSGET_H_ */
