03b53d93e4a103c86b6563ab3dd20e5938e3ec79
berman <berman>
1997-11-26 21:13:42 +0000

    minimal working version

--- src/ups_setup.c
+++ src/ups_setup.c
@@ -27,7 +27,8 @@
 #include "upsugo.h"
 #include "upsutl.h"
 #include "upsmat.h"
-
+#include "upsact.h"
+#include "ups_main.h"
 /*
  * Definition of public variables.
  */
@@ -38,13 +39,14 @@
  */
 
 static t_upslst_item *setup_core(const t_upsugo_command * const a_command_line,
-				 const FILE * const a_temp_file);
+				 const FILE * const a_temp_file,
+				 const int a_ups_command);
 
 /*
  * Definition of global variables.
  */
 
-
+extern t_cmd_info g_cmd_info[];
 /*
  * Definition of public functions.
  */
@@ -59,12 +61,12 @@ static t_upslst_item *setup_core(const t_upsugo_command * const a_command_line,
  * Return: none
  */
 void ups_setup( const t_upsugo_command * const a_command_line,
-		const FILE * const a_temp_file)
+		const FILE * const a_temp_file, const int a_ups_command)
 {
   t_upslst_item *mproduct_list = NULL;
 
   /* now find the desired instance and translate actions to the temp file */
-  mproduct_list = setup_core(a_command_line, a_temp_file);
+  mproduct_list = setup_core(a_command_line, a_temp_file, a_ups_command);
 
   /* check if we got an error */
   if (UPS_ERROR == UPS_SUCCESS) {
@@ -72,10 +74,11 @@ void ups_setup( const t_upsugo_command * const a_command_line,
 
     /* write statistics information */
     if (mproduct_list) {
-      upsutl_statistics(mproduct_list, "setup");
+      upsutl_statistics(mproduct_list, g_cmd_info[a_ups_command].cmd);
     }
   }
-
+  /* clean up the matched product list */
+  mproduct_list = upsutl_free_matched_product_list(&mproduct_list);
 }
 
 /*
@@ -94,11 +97,13 @@ void ups_setup( const t_upsugo_command * const a_command_line,
  * Return: <return>
  */
 static t_upslst_item *setup_core(const t_upsugo_command * const a_command_line,
-				 const FILE * const a_temp_file)
+				 const FILE * const a_temp_file,
+				 const int a_ups_command)
 {
   t_upslst_item *mproduct_list = NULL;
   t_upstyp_matched_product *mproduct = NULL;
   t_upstyp_matched_instance *minst = NULL;
+  t_upslst_item *cmd_list;
   char *dummy = NULL;
   int need_unique = 1;
 
@@ -108,14 +113,38 @@ static t_upslst_item *setup_core(const t_upsugo_command * const a_command_line,
   if (mproduct_list && (UPS_ERROR == UPS_SUCCESS)) {
     /* get the product to be set up */
     mproduct = (t_upstyp_matched_product *)mproduct_list->data;
-    minst = (t_upstyp_matched_instance *)(mproduct->minst_list->data);
 
-    /* check if this product is authorized to be setup on this node */
-    if (upsutl_is_authorized(minst, mproduct->db_info, dummy)) {
-      /* Check if we need to unsetup this product first.  */
-      printf("we got to here\n");      
-    } else {
-      upserr_add(UPS_NOT_AUTH, UPS_FATAL, mproduct->product);
+    /* make sure an instance was matched before proceeding */
+    if (mproduct->minst_list) {
+      minst = (t_upstyp_matched_instance *)(mproduct->minst_list->data);
+
+      /* check if this product is authorized to be setup on this node */
+      if (upsutl_is_authorized(minst, mproduct->db_info, dummy)) {
+	/* Check if we need to unsetup this product first.  */
+	if (a_command_line->ugo_k == 0) {
+	  /* the command line says it is ok to do the unsetup */
+	  cmd_list = upsact_get_cmd((t_upsugo_command *)a_command_line,
+				    mproduct, g_cmd_info[e_unsetup].cmd);
+	  if (UPS_ERROR == UPS_SUCCESS) {	  
+	    /* Now output all the actions to the file */
+	    upsact_process_commands(cmd_list, a_temp_file);
+	  }
+	  /* now clean up the memory that we used */
+	  upsact_cleanup(cmd_list);
+	}
+	if (UPS_ERROR == UPS_SUCCESS) {	  
+	  /* Now process the setup actions */
+	  cmd_list = upsact_get_cmd((t_upsugo_command *)a_command_line,
+				    mproduct, g_cmd_info[a_ups_command].cmd);
+	  if (UPS_ERROR == UPS_SUCCESS) {
+	    upsact_process_commands(cmd_list, a_temp_file);
+	  }
+	  /* now clean up the memory that we used */
+	  upsact_cleanup(cmd_list);
+	}
+      } else {
+	upserr_add(UPS_NOT_AUTH, UPS_FATAL, mproduct->product);
+      }
     }
   } 
 
