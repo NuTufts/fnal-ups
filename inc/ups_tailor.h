704ab0f9caa3f520ee93a76a2f1838772c8e6c2c
berman <berman>
1997-12-04 17:02:04 +0000

    tailor command

---
+++ inc/ups_tailor.h
@@ -0,0 +1,56 @@
+/************************************************************************
+ *
+ * FILE:
+ *       ups_tailor.h
+ * 
+ * DESCRIPTION: 
+ *       Define all necessary command prototypes etc.
+ *
+ * AUTHORS:
+ *       Eileen Berman
+ *       David Fagan
+ *       Lars Rasmussen
+ *
+ *       Fermilab Computing Division
+ *       Batavia, Il 60510, U.S.A.
+ *
+ * MODIFICATIONS:
+ *       18-Aug-1997, EB, first
+ *
+ ***********************************************************************/
+
+#ifndef _UPS_TAILOR_H_
+#define _UPS_TAILOR_H_
+
+/* standard include files, if needed for .h file */
+
+/* ups specific include files, if needed for .h file */
+#include "upsugo.h"
+
+/*
+ * Constans.
+ */
+
+/*
+ * Types.
+ */
+
+/*
+ * Declaration of public functions.
+ */
+void ups_tailor( const t_upsugo_command * const a_command_line, 
+		 const FILE * const a_temp_file, 
+		 const int a_ups_command);
+void ups_tailor_cleanup(void);
+void ups_tailor_update_nodes(void);
+
+/*
+ * Declaration of private globals.
+ */
+
+/*
+ * Declarations of public variables.
+ */
+
+
+#endif /* _UPS_TAILOR_H_ */
