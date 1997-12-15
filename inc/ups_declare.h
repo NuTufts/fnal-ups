f02b861139b3789f53587ab90e46e7fcad4f005d
fagan <fagan>
1997-12-15 17:35:16 +0000

    first proro ups_declare

---
+++ inc/ups_declare.h
@@ -0,0 +1,41 @@
+/************************************************************************
+ *
+ * FILE:
+ *       ups_declare.h
+ * 
+ * DESCRIPTION: 
+ *       Prototypes etc., for instance matching
+ *
+ * AUTHORS:
+ *       Eileen Berman
+ *       David Fagan
+ *       Lars Rasmussen
+ *
+ *       Fermilab Computing Division
+ *       Batavia, Il 60510, U.S.A.
+ *
+ * MODIFICATIONS:
+ *       Mon Dec 15, Djf, First
+ ***********************************************************************/
+
+#ifndef _UPS_DECLARE_H_
+#define _UPS_DECLARE_H_
+
+/* ups specific include files, if needed for .h file */
+#include "upsugo.h"
+
+/*
+ * Constans.
+ */
+
+/*
+ * Types.
+ */
+
+/*
+ * Declaration of public functions.
+ */
+
+t_upslst_item *ups_declare(t_upsugo_command * const a_command_line);
+
+#endif /* _UPS_DECLARE_H_ */
