6975d80467a0369138f8377c372ec9e87ef05e69
berman <berman>
1998-01-06 23:11:24 +0000

    add prototype

--- inc/upsact.h
+++ inc/upsact.h
@@ -72,6 +72,7 @@ int upsact_print( t_upsugo_command * const ugo_cmd,
 		  const char * const sopt );
 t_upsact_cmd *upsact_parse_cmd( const char * const cmd_str );
 void upsact_cleanup( t_upslst_item *dep_list );
+t_upslst_item *upsact_check_files( const char * const a_cmd);
 void upsact_free_upsact_cmd( t_upsact_cmd * const act_cmd );
 void upsact_print_cmd( const t_upsact_cmd * const cmd_cur );
 void upsact_print_item( const t_upsact_item *const p_cur, const char * const sopt );
