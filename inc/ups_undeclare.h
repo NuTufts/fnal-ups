ad1797c4f12e9209e9db074bacbbaa75ed8a9758
fagan <fagan>
1998-01-21 13:33:58 +0000

    modifications to finish command processing.

--- inc/ups_undeclare.h
+++ inc/ups_undeclare.h
@@ -36,6 +36,9 @@
  * Declaration of public functions.
  */
 
-void ups_undeclare(t_upsugo_command * const a_command_line);
+void ups_undeclare(t_upsugo_command * const a_command_line,
+                    const FILE * const a_temp_file,
+                    const int a_ups_command);
+
 
 #endif /* _UPS_UNDECLARE_H_ */
