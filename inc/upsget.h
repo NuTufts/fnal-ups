014373eff029d573f56497a1b40ed36589264e0f
fagan <fagan>
1998-01-30 21:46:26 +0000

    changed to voids...

--- inc/upsget.h
+++ inc/upsget.h
@@ -56,16 +56,16 @@ char *upsget_OS_flavor(const t_upstyp_db * const,
 char *upsget_extended(const t_upstyp_db * const,
                       const t_upstyp_matched_instance * const ,
                       const t_upsugo_command * const );
-char *upsget_remall(const FILE * const,
+void upsget_remall(const FILE * const,
                     const t_upsugo_command * const);
-char *upsget_allout(const FILE * const,
+void upsget_allout(const FILE * const,
                     const t_upstyp_db * const,
                     const t_upstyp_matched_instance * const,
                     const t_upsugo_command * const);
 char *upsget_envstr(const t_upstyp_db * const,
                     const t_upstyp_matched_instance * const ,
                     const t_upsugo_command * const );
-char *upsget_envout(const FILE * const,
+void upsget_envout(const FILE * const,
                     const t_upstyp_db * const,
                     const t_upstyp_matched_instance * const,
                     const t_upsugo_command * const);
