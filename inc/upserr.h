8c8a4a69a05183e6d9aa0e8d4b94ea6a2f678b69
berman <berman>
1998-02-23 16:10:04 +0000

    remov file_not_found error

--- inc/upserr.h
+++ inc/upserr.h
@@ -83,8 +83,7 @@
 #define UPS_INVALID_SPECIFICATION 40 
 #define UPS_NO_PRODUCT_FOUND    41 
 #define UPS_DUPLICATE_INSTANCE  42
-#define UPS_FILE_NOT_FOUND      43
-#define UPS_NERR                44      /*  this one must always be last */
+#define UPS_NERR                43      /*  this one must always be last */
 
 #define UPS_FATAL           "ERROR"
 #define UPS_WARNING         "WARNING"
