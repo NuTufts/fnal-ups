675eb9af7e7c9b41d498d5e8faeb0e240a44fe38
rasmus <rasmus>
1998-03-17 21:19:30 +0000

    added jounal file handling in upsfil

--- inc/upsfil.h
+++ inc/upsfil.h
@@ -56,7 +56,6 @@ void              g_print_product( t_upstyp_product * const prod_ptr );
 #define CONFIG_FILE     "dbconfig"
 #define CONFIG_FILE_LEN 8
 #define CONFIG_SIZE     UPS_FILES_LEN + CONFIG_FILE_LEN + 1
-#define JOURNAL         1 /* mark file for write only wait for journal write */
 
 /* enum of known file types (changes here should be reflected in cfilei) */
 enum e_ups_file {
