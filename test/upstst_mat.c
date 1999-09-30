d0b74031a407d1482245b710241d0eaa5e160aad
baisley <baisley>
1999-09-30 22:04:09 +0000

    Add void casts to make the compiler shut up

--- test/upstst_mat.c
+++ test/upstst_mat.c
@@ -76,7 +76,7 @@ if (outfile)                                    /* don't use stdout */
 else
    {ofd = stdout;}
 stdout_dup = dup(STDOUT_FILENO);                /* dup stdout */
-fflush(stdout);                                 /* clear output buffer */
+(void) fflush(stdout);                          /* clear output buffer */
 status = dup2(fileno(ofd),STDOUT_FILENO);       /* reset it to output file */
 
 /* call the real routine
@@ -87,14 +87,14 @@ while (uc = upsugo_next(argc,argv,UPSTST_ALLOPTS))	/* for all commands */
    {
    if (UPS_ERROR != UPS_SUCCESS)			/* error on ugo_next */
        {
-       fprintf(stderr,"upsugo_next failed: %s\n", g_error_ascii[UPS_ERROR]);  
+       (void) fprintf(stderr,"upsugo_next failed: %s\n", g_error_ascii[UPS_ERROR]);  
        upserr_output(); upserr_clear();
        return (0);
        }
    mp = upsmat_instance(uc,dblist,unique);
    if (UPS_ERROR != estatus)                    	/* error? */
        {
-       fprintf(stderr,"%s: %s, %s: %s\n","actual status",
+       (void) fprintf(stderr,"%s: %s, %s: %s\n","actual status",
           g_error_ascii[UPS_ERROR],"expected status", g_error_ascii[estatus]);
        if (UPS_ERROR) { upserr_output(); upserr_clear(); }
        }
@@ -105,15 +105,15 @@ while (uc = upsugo_next(argc,argv,UPSTST_ALLOPTS))	/* for all commands */
 /* dump the output to specified file and compare
    --------------------------------------------- */
 
-fflush(stdout);                                 /* flush buffer */
-dup2(stdout_dup,STDOUT_FILENO);                 /* reset stdout */
-close(stdout_dup);                              /* close files*/
-if(fileno(ofd) != STDOUT_FILENO) fclose(ofd);
+(void) fflush(stdout);                                 /* flush buffer */
+(void) dup2(stdout_dup,STDOUT_FILENO);                 /* reset stdout */
+(void) close(stdout_dup);                              /* close files*/
+if(fileno(ofd) != STDOUT_FILENO) (void) fclose(ofd);
 
 if (difffile && outfile)
    {
-   sprintf (diffcmd,"diff %s %s",difffile,outfile);
-   if (system(diffcmd)) printf("files %s %s differ\n",difffile,outfile);
+   (void) sprintf (diffcmd,"diff %s %s",difffile,outfile);
+   if (system(diffcmd)) (void) printf("files %s %s differ\n",difffile,outfile);
    }
 
 if (database) upsmem_free(dbaddr);
@@ -142,15 +142,15 @@ for (prod_ptr = (t_upslst_item *)mp; prod_ptr; prod_ptr = prod_ptr->next)
    {
    prod = (t_upstyp_matched_product *) prod_ptr->data;
    db = (t_upstyp_db *) prod->db_info;
-   if (db) printf("D:NAME=%s\n", db->name);
+   if (db) (void) printf("D:NAME=%s\n", db->name);
    for (inst_ptr = prod->minst_list; inst_ptr; inst_ptr = inst_ptr->next)
       {
       inst = (t_upstyp_matched_instance *) inst_ptr->data;
       if (inst->chain)
          {
-         printf("C:PRODUCT=%15s, VERSION=%10s, ", inst->chain->product,
+         (void) printf("C:PRODUCT=%15s, VERSION=%10s, ", inst->chain->product,
              inst->chain->version);
-         printf("FLAVOR=%12s, QUALIFIERS=%s, CHAIN=%s\n", inst->chain->flavor,
+         (void) printf("FLAVOR=%12s, QUALIFIERS=%s, CHAIN=%s\n", inst->chain->flavor,
              inst->chain->qualifiers, inst->chain->chain);
          if (inst->xtra_chains)
 	    {
@@ -159,27 +159,27 @@ for (prod_ptr = (t_upslst_item *)mp; prod_ptr; prod_ptr = prod_ptr->next)
  	    for (clist = inst->xtra_chains; clist; clist=clist->next)
 		{
 		cinst = (t_upstyp_instance *)clist->data; 
-                printf("C:PRODUCT=%15s, VERSION=%10s, ", cinst->product,
+                (void) printf("C:PRODUCT=%15s, VERSION=%10s, ", cinst->product,
                     cinst->version);
-                printf("FLAVOR=%12s, QUALIFIERS=%s, CHAIN=%s\n", 
+                (void) printf("FLAVOR=%12s, QUALIFIERS=%s, CHAIN=%s\n", 
 		    cinst->flavor, cinst->qualifiers, cinst->chain);
 		}
 	    }
          }
       if (inst->version)
          {
-         printf("V:PRODUCT=%15s, VERSION=%10s, ", 
+         (void) printf("V:PRODUCT=%15s, VERSION=%10s, ", 
 		inst->version->product ? inst->version->product : "(null)" ,
 		inst->version->version ? inst->version->version : "(null)" );
-         printf("FLAVOR=%12s, QUALIFIERS=%s\n", inst->version->flavor,
+         (void) printf("FLAVOR=%12s, QUALIFIERS=%s\n", inst->version->flavor,
              inst->version->qualifiers);
          }
       if (inst->table)
          {
-         printf("T:PRODUCT=%15s, VERSION=%10s, ", 
+         (void) printf("T:PRODUCT=%15s, VERSION=%10s, ", 
 		inst->table->product ? inst->table->product : "(null)",
 		inst->table->version ? inst->table->version : "(null)");
-         printf("FLAVOR=%12s, QUALIFIERS=%s\n", inst->table->flavor,
+         (void) printf("FLAVOR=%12s, QUALIFIERS=%s\n", inst->table->flavor,
              inst->table->qualifiers);
          }
       }
