d0d136b0f7963f5e2075614daa059f617c83ca57
baisley <baisley>
1999-11-12 23:01:54 +0000

    DON'T remove option k from the list for exist

--- src/upsact.c
+++ src/upsact.c
@@ -394,7 +394,7 @@ t_cmd_info g_cmd_info[] = {
   {e_copy,        "copy",        "?A:b:cCdD:f:g:G:H:m:M:noO:p:q:r:tT:u:U:vVWXz:Z0123", 0x00000010, e_invalid_action},
   {e_declare,     "declare",     "?A:b:cCdD:f:g:H:Lm:M:noO:q:r:tT:u:U:vVz:Z0123", 0x00000010, e_declare},
   {e_depend,      "depend",      "?cdg:f:H:jK:lm:M:noq:r:RtU:vVz:Z0123", 0x00000010, e_invalid_action},
-  {e_exist,       "exist",       "?B:cdef:g:H:jm:M:noO:Pq:r:tU:vVz:Z0123", 0x00000010, e_invalid_action},
+  {e_exist,       "exist",       "?B:cdef:g:H:jkm:M:noO:Pq:r:tU:vVz:Z0123", 0x00000010, e_invalid_action},
   {e_modify,      "modify",      "?aA:E:f:H:m:M:Np:q:r:T:U:vVx:z:Z", 0x00000010, e_invalid_action},
   {e_start,       "start",       "?cdf:g:H:m:M:noO:Pq:r:stU:vVwz:Z0123", 0x00000010, e_invalid_action},
   {e_stop,        "stop",        "?cdf:g:H:m:M:noO:Pq:r:stU:vVz:Z0123", 0x00000010, e_invalid_action},
