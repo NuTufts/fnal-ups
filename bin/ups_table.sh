37eeb42302517793fac84553a1597caf0bec8582
ron <ron>
2009-09-08 16:27:05 +0000

    make options more like ups options

--- bin/ups_table.sh
+++ bin/ups_table.sh
@@ -11,16 +11,70 @@ USAGE="\
 table file to add qualified (or build) and non-qualified dependencies
 and qualified (or build) and non-qualified env. variables.
 
-  usage: `basename $0` [options] <prod|table> [ver]
+   usage: `basename $0` [opt] <cmd=add_fq>
+ example: `basename $0` add_fq
 options:
-  -q
-...
+  -f flavor
+  -q qualifiers
+  -m table file
+ --deps=
+ --vars=
 "
+set_opt()
+{
+    opts_w_args='m|f|q|deps|vars'
+    opts_wo_args='v'
+    do_opt="\
+      case \$opt in
+      \\?|h|help)    echo \"\$USAGE\"; exit 0;;
+      $opts_wo_args) vv=opt_\`echo \$opt |sed -e 's/-/_/g'\`
+                     eval xx=\\\${\$vv-0};eval \"\$vv=\`expr \$xx + 1\`\";;
+      $opts_w_args)  if   [ \"\${rest-}\" != '' ];then arg=\$rest; rest=
+                     elif [      $#      -ge 1  ];then arg=\$1; shift
+                     else  echo \"option \$opt requires argument\"; exit 1; fi
+                     eval opt_\`echo \$opt|sed -e 's/-/_/g'\`=\"'\$arg'\"
+                     opts=\"\$opts '\$arg'\";;      # tricky part Aa
+      *) echo \"invalid option: \$opt\" 2>&1;echo \"\$USAGE\" 2>&1; exit 1;;
+      esac"
+    while op=`expr "${1-}" : '-\(.*\)'`;do
+        shift
+        if xx=`expr "$op" : '-\(.*\)'`;then
+            if   opt=`expr     "$xx" : '\([^=]*\)='`;then
+                set ${-+-$-} -- "`expr "$xx" : '[^=]*=\(.*\)'`" "$@"
+            else opt=$xx; fi
+            opts="${opts-} '--$opt'"      # tricky part Ab
+            eval "$do_opt"
+        else
+            while opt=`expr "$op" : '\(.\)'`;do
+                opts="${opts-} '-$opt'"      # tricky part Ac
+                rest=`expr "$op" : '.\(.*\)'`
+                eval "$do_opt"
+                op=$rest
+            done
+        fi
+    done
+}
 
-#if [ $# -lt 1 -o $# -gt 2 ];then echo "$USAGE"; exit; fi
+echov() { if [ "${opt_v-}" ];then echo "$@" >&2; fi; }
 
+#-------------------------------------------------------------------------
 
+build_fq_ere()
+{   list=$1
+    pre_list=${2-}
+    for qq in `echo $list | tr : ' '`;do
+        new_list=`echo :$list: | sed "s/:$qq:/:/;s/^://;s/:$//"`
+        if [ "${opt_all-}" ];then echo "$pre_list$qq"; fi
+        if [ "$new_list" ];then
+            yy=`build_fq_ere "$new_list" "$pre_list${qq}:"`
+            echo "$yy"
+        else
+            if [ "${opt_all-}" = '' ];then echo "$pre_list$qq"; fi
+        fi
+    done
+}
 
+#-------------------------------------------------------------------------
 
 add_fq()
 {
@@ -28,13 +82,19 @@ add_fq()
     set_TBLF
     set_FLV
     set_QUAL
-    if list_fq | grep "$FLV \"$QUAL\"";then
-        echo "flv/qual combination already exists"
+    if [ "$QUAL" ];then
+        qual_ere="(`build_fq_ere $QUAL | sed -n 'H;${x;s/\n/|/g;s/^|//;p;}'`)"
+    else
+        qual_ere=
+    fi
+    echov "qual_ere=$qual_ere"
+    if list_fq | egrep "$FLV \"$qual_ere\"";then
+        echov "flv/qual combination already exists"
         awk_fq='{print;}'
     else
         awk_fq="BEGIN{IGNORECASE=1;}
 { print; }
-/^GROUP:/{ printf \"\
+/^GROUP:/{ printf \"\n\
 FLAVOR = $FLV\nQUALIFIERS = \\\"$QUAL\\\"\n\
     ACTION = setup\n\
         exeActionRequired(\\\"setup__\\\")\n\
@@ -45,8 +105,7 @@ FLAVOR = $FLV\nQUALIFIERS = \\\"$QUAL\\\"\n\
         sed_up=''
         sed_dn=''
         IFSsav=$IFS IFS=,; for dep in $opt_deps;do IFS=$IFSsav
-            dep=`echo $dep`
-            echo "dep=>$dep<"
+            dep=`echo $dep`  # removes potential space next to comma IFS
             sed_up="$sed_up        setupRequired( \\\"$dep\\\" )\\n"
             # unsetup in opposite order
             sed_dn="\\n        unsetupRequired( \"$dep\" )$sed_dn"
@@ -70,7 +129,7 @@ FLAVOR = $FLV\nQUALIFIERS = \\\"$QUAL\\\"\n\
     if [ "${opt_vars-}" ];then
         vars=''
         IFSsav=$IFS IFS=,; for vv in $opt_vars;do IFS=$IFSsav
-            vvar=`expr "$vv" : '\([^=]*\)'`
+            vvar=`expr "$vv" : ' *\([^=]*\)'` # recall, there may be a " " after a "," IFS
             vval=`expr "$vv" : '[^=]*=\(.*\)'`
             vars="$vars        envSet( $vvar, $vval )\\n"
         done
@@ -148,59 +207,24 @@ list_deps()
 #------------------------------------------------------------------------------
 set_TBLF()
 {   if [ "${TBLF-}" ];then return; fi
-    if [ "${opt_tblf-}" ];then TBLF=$opt_tblf
+    if [ "${opt_m-}" ];then TBLF=$opt_m
     else                      TBLF=$UPS_DIR/ups/generic.table; fi
 }
 set_FLV()
 {   if [ "${FLV-}" ];then return; fi
-    if [ "${opt_flv-}" ];then FLV=$opt_flv
+    if [ "${opt_f-}" ];then FLV=$opt_f
     else                      FLV=`ups flavor`; fi
 }
 set_QUAL()
 {   if [ "${QUAL-}" ];then return; fi
-    if [ "${opt_qual-}" ];then QUAL=$opt_qual
+    if [ "${opt_q-}" ];then QUAL=$opt_q
     else                       QUAL=; fi
-}
-set_opt()
-{
-    opts_w_args='tblf|flv|qual|deps|vars'
-    do_opt="\
-    case \$opt in
-    \\?|h|help)    echo \"\$USAGE\"; exit 0;;
-    $opts_w_args)  if   [ \"\${rest-}\" != '' ];then arg=\$rest; rest=
-                   elif [      $#      -ge 1  ];then arg=\$1; shift
-                   else  echo \"option \$opt requires argument\"; exit 1; fi
-                   eval opt_\`echo \$opt|sed -e 's/-/_/g'\`=\"'\$arg'\"
-                   opts=\"\$opts '\$arg'\";;      # tricky part Aa
-    *) echo \"invalid option: \$opt\" 2>&1;echo \"\$USAGE\" 2>&1; exit 1;;
-    esac"
-    while op=`expr "${1-}" : '-\(.*\)'`;do
-        shift
-        if xx=`expr "$op" : '-\(.*\)'`;then
-            if   opt=`expr     "$xx" : '\([^=]*\)='`;then
-                set ${-+-$-} -- "`expr "$xx" : '[^=]*=\(.*\)'`" "$@"
-            else opt=$xx; fi
-            opts="${opts-} '--$opt'"      # tricky part Ab
-            eval "$do_opt"
-        else
-            while opt=`expr "$op" : '\(.\)'`;do
-                opts="${opts-} '-$opt'"      # tricky part Ac
-                rest=`expr "$op" : '.\(.*\)'`
-                eval "$do_opt"
-                op=$rest
-            done
-        fi
-    done
 }
 #------------------------------------------------------------------------------
 
 if [ "${1-}" = -x ];then set -x;shift; fi
 if [ "${1-}" ];then
     set -u
-    USAGE="\
-   usage: `basename $0` [opt] <cmd=add_fq>
- example: `basename $0` add_fq
-"
     cmd=$1; shift
     case $cmd in
     add_fq)    add_fq "$@";;
