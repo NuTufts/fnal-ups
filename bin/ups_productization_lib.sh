e9372634008602a6fa12192e4dcca87a0df79e6b
ron <ron>
2009-12-06 04:48:05 +0000

    make j_opt

--- bin/ups_productization_lib.sh
+++ bin/ups_productization_lib.sh
@@ -278,7 +278,8 @@ generic_configure_build()
     # eval for flags and $PREFIX in $opt_configure
     cmd "$flags ../configure --prefix=$PREFIX ${opt_configure-}"
     qual_unO_Makefile
-    make
+    test -f /prof/cpuinfo && j_opt=-j`grep processor /proc/cpuinfo | wc -l`
+    make ${j_opt-} ${opt_make-}
 }   # generic_configure_build
 generic_configure_install()
 {
