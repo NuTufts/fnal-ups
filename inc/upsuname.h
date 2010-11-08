132f7c41feaa8ee6b043a6596a36fddb828dc27a
garren <garren>
2010-11-08 18:14:37 +0000

    add missing declarations

--- inc/upsuname.h
+++ inc/upsuname.h
@@ -1,5 +1,8 @@
 
 void ups_append_OS(char *flavor);
+void ups_append_MACHINE(char *buf);
 void ups_append_release(char *flavor);
+void ups_make_default_quals_optional();
 char *ups_get_default_quals();
 char *ups_get_default_host();
+int ups_64bit_check();
