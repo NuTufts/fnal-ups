e9feda81764373815f32e473a6087a0fee1ed628
garren <garren>
2013-04-16 19:42:48 +0000

    adding ups database directory to ups active output

--- src/ups_active.pl
+++ src/ups_active.pl
@@ -3,7 +3,7 @@
 # this little script just lists the ups products which are currently setup.
 
 printf( "Active ups products:\n");
-open(QL, "printenv | grep SETUP | sort |") || die "Failed: $!\n";
+open(QL, "printenv | grep SETUP | grep -v SETUPS_DIR | sort |") || die "Failed: $!\n";
 while( $line = <QL> ) {
     chop($line);
     @words=split(/=/,$line);
@@ -11,7 +11,9 @@ while( $line = <QL> ) {
     @pl=split(/\s+/,$words[1]);
     printf("%-16s  %-15s -f %-20s", $pl[0],$pl[1],$pl[3]);
     if($#pl > 5 ) {
-        printf(" -q %-15s",$pl[7]);
+        printf(" -q %-15s -z %s",$pl[7],$pl[5]);
+    } else {
+        printf("%s -z %s",' ' x 19,$pl[5]);
     }
     printf("\n");
 }
